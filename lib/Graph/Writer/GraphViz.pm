156eae27a5e0e539c26fbae65eadd6a93a6797fe
劉康民 <gugod@gugod.org>
2004-06-27 08:46:49 +0000

    Import of GUGOD/Graph-Writer-GraphViz-0.01 from CPAN.
    gitpan-cpan-distribution: Graph-Writer-GraphViz
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         GUGOD/Graph-Writer-GraphViz-0.01.tar.gz
    gitpan-cpan-author:       GUGOD
    gitpan-cpan-maturity:     released

---
+++ lib/Graph/Writer/GraphViz.pm
@@ -0,0 +1,87 @@
+package Graph::Writer::GraphViz;
+use strict;
+use GraphViz;
+use Graph::Writer;
+use vars qw(@ISA);
+@ISA = qw(Graph::Writer);
+
+our $VERSION = '0.01';
+
+my $format = 'png';
+
+sub _init  {
+    my ($self,%param) = @_;
+    $self->SUPER::_init();
+    $format = $param{-format} || 'png';
+}
+
+sub _write_graph {
+    my ($self, $graph, $FILE) = @_;
+    my $grvz = $self->graph2graphviz($graph);
+    my $as_fmt = 'as_' . $format;
+    $grvz->$as_fmt($FILE);
+}
+
+sub graph2graphviz {
+    my ($self,$g) = @_;
+    my $r = GraphViz->new(directed=>$g->directed);
+    my @v = $g->vertices;
+    my @e = $g->edges;
+    $r->add_node($_) for @v;
+    $r->add_edge(@e[2*$_,2*$_+1]) for 0..@e/2-1;
+    return $r;
+}
+
+
+1;
+__END__
+
+=head1 NAME
+
+Graph::Writer::GraphViz - GraphViz Writer for Graph object
+
+=head1 SYNOPSIS
+
+
+  my @v = qw/Alice Bob Crude Dr/;
+  my $g = Graph->new(@v);
+
+  my $wr = Graph::Writer::GraphViz->new(-format => 'dot');
+  $wr->write_graph($g,'/tmp/graph.simple.dot');
+
+  my $wr_png = Graph::Writer::GraphViz->new(-format => 'png');
+  $wr_png->write_graph($g,'/tmp/graph.simple.png');
+
+=head1 DESCRIPTION
+
+B<Graph::Writer::GraphViz> is a class for writing out a Graph
+object with GraphViz module. All GraphViz formats should
+be supported without a problem.
+
+=head1 METHODS
+
+=head2 new()
+
+Unlike other B<Graph::Writer> modules, this module
+provide an extra parameter '-format' to new() method,
+in order to save different format. Please see
+the SYNOPSIS for example usage.
+
+Valid format depends on those GraphViz B<as_fmt> methods on your
+system, like, 'gif' if you have 'as_gif', 'text' if you can do
+'as_text'.
+
+=head1 SEE ALSO
+
+L<Graph>, L<Graph::Writer>, L<GraphViz>
+
+=head1 COPYRIGHT
+
+Copyright 2004 by Kang-min Liu <gugod@gugod.org>.
+
+This program is free software; you can redistribute it and/or
+modify it under the same terms as Perl itself.
+
+See <http://www.perl.com/perl/misc/Artistic.html>
+
+=cut
---
+++ t/1.simple.t
@@ -0,0 +1,29 @@
+#!/usr/bin/env perl -w
+
+use strict;
+use Test::Simple tests => 1;
+use IO::All;
+use Graph;
+use Graph::Writer::GraphViz;
+
+my @v = qw/Alice Bob Crude Dr/;
+my $g = Graph->new(@v);
+
+my $wr = Graph::Writer::GraphViz->new(-format => 'dot');
+$wr->write_graph($g,'/tmp/graph.simple.dot');
+
+$/ = undef;
+my $g1 = <DATA>;
+my $g2 = io('/tmp/graph.simple.dot')->slurp;
+ok($g1 eq $g2);
+unlink('/tmp/graph.simple.dot');
+
+__DATA__
+digraph test {
+	node [label="\N"];
+	graph [bb="0,0,290,52"];
+	Alice [label=Alice, pos="30,26", width="0.83", height="0.50"];
+	Bob [label=Bob, pos="105,26", width="0.75", height="0.50"];
+	Crude [label=Crude, pos="184,26", width="0.94", height="0.50"];
+	Dr [label=Dr, pos="263,26", width="0.75", height="0.50"];
+}
